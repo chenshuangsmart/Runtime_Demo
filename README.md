935bdb4212dc62b18ca154ce0df10bcab4703b2a
陈双 <chenshuang@banggood.com>
2018-08-27 08:47:31 +0800

    关于 runtime isa 的相关介绍
    关于 runtime isa 的相关介绍

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Runtime_Demo
  关于 runtime 的详细介绍和对应例子
+1.runtime_API
+关于 runtime 相关 API 的详细介绍
+
+2.runtime_exchangeMethod
+关于使用 runtime进行方法的交互例子
+
+3.runtime_interview1
+关于使用 runtime 的一些介绍
+
+5.runtime_isa_desc
+关于 isa 字段的详解例子
---
+++ rumtime_isa_desc/rumtime_isa_desc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  rumtime_isa_desc
+//
+//  Created by cs on 2018/8/21.
+//  Copyright © 2018年 cs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
